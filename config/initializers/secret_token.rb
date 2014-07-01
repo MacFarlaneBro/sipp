f48f9944027e5a3c29cbad39aff0196107cc7560
Charlie Brodie <charliembrodie@gmail.com>
2014-07-01 15:33:54 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DadSite::Application.config.secret_key_base = '0ae9d034edda9d094de915f8d3d8c848fa5b41819deb43776ce21969dce400116a15aed947dc3360e25bedff6c14e02c7e9638d6483ff0261cc56978ca1321cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DadSite::Application.config.session_store :cookie_store, key: '_dad_site_session'
