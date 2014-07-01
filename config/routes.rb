7bfe42a704091bebd7c15ed62bbd437667b76457
Charlie Brodie <charliembrodie@gmail.com>
2014-07-01 17:03:42 +0100

    added a posting functionality and a commenting functionality, rudimentary stuff but hopefully I should be able to get stuff up and running fairly swiftly now what with some of the design done and all.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 DadSite::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
+  resources :comments
+
+  resources :posts do
+    resources :comments, :only => [:create]
+  end
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
